58eec5fc248597542aaeb222fc95b63e6170ffea
Dawit <dawitgzb@yahoo.com>
2018-02-15 15:01:25 -0500

    wrote helper function to check row in table list

--- functional_tests.py
+++ functional_tests.py
@@ -9,6 +9,11 @@ class NewVisitorTest(unittest.TestCase):
 	def setUp(self):
 		self.browser = webdriver.Firefox();
 
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id("id_list_table")
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+
 	def tearDown(self):
 		self.browser.quit()
 
@@ -35,9 +40,7 @@ class NewVisitorTest(unittest.TestCase):
 		input_box.send_keys(Keys.ENTER)
 		time.sleep(1)
 
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 		# There is still a text box inviting her to add another item and she enters
 		# "Use peacock feathers to make a fly"
@@ -47,9 +50,8 @@ class NewVisitorTest(unittest.TestCase):
 		time.sleep(1)
 
 		# The page updates again, and now shows both items on her to-do list
-		rows = self.browser.find_elements_by_id('id_list_table')
-		self.assertIn('1: Buy peacock feathers',[row.text for row in rows])
-		self.assertIn('2. Use peacock feathers to make a fly', [row.text for row in rows])
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.check_for_row_in_list_table('2. Use peacock feathers to make a fly')
 
 		# Edith wonders whether the site will remember her list. Then she notices
 		# that the site has generated a unique URL for her -- there is an explanatory
